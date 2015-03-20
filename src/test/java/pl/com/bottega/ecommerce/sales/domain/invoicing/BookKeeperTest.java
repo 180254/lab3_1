3e9cfef698571508adb31de365e2ffa6f3b49c5d
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2015-03-20 10:56:52 +0100

    Added new test for BookKeeper - test with state check - if invoice request has two items, tax should be calculated twice.

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -62,7 +62,23 @@ public class BookKeeperTest {
 
 	@Test
 	public final void testIssuance_testState_invoiceWithTwoItems_checkTaxPolicyInvokeCounter() {
-		fail("Not yet implemented");
+		ClientData clientData = new ClientData(Id.generate(), "not important");
+		Invoice invoice = new Invoice(Id.generate(), clientData);
+		InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
+		Money price = new Money(10);
+		ProductData productData = new ProductData(Id.generate(), price, "not important", ProductType.DRUG, new Date());
+		RequestItem item = new RequestItem(productData, 0, new Money(10));
+		invoiceRequest.add(item);
+		invoiceRequest.add(item);
+
+		InvoiceFactory invoiceFactory = Mockito.mock(InvoiceFactory.class);
+		TaxPolicy taxPolicy = Mockito.mock(TaxPolicy.class);
+		Mockito.when(invoiceFactory.create(invoiceRequest.getClientData())).thenReturn(invoice);
+		Mockito.when(taxPolicy.calculateTax(ProductType.DRUG, price)).thenReturn(new Tax(new Money(1d), "not important"));
+		
+		bookKeeper = new BookKeeper(invoiceFactory);
+		Invoice newInvoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
+		Mockito.verify(taxPolicy, Mockito.times(2)).calculateTax(Mockito.any(ProductType.class), Mockito.any(Money.class));
 	}
 
 }
