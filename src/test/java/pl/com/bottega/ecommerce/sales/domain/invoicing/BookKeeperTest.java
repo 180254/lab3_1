2100450c93b7d876cb2cb55f7d3c7e2499560f4e
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2015-03-20 17:48:59 +0100

    Refactored test: testIssuance - testState - methodFactoryFromInvoiceFactoryShouldBeAlwaysInvokedOnce. Used test builders.

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -3,8 +3,6 @@ package pl.com.bottega.ecommerce.sales.domain.invoicing;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-import java.util.Date;
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -12,9 +10,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
-import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
-import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
@@ -117,25 +112,25 @@ public class BookKeeperTest {
 	}
 
 	@Test
-	public final void testIssuance_testState_methodFactoryFromInvoiceFactoryShouldBeInvokedOneTime() {
-		ClientData clientData = new ClientData(Id.generate(), "not important");
-		Invoice invoice = new Invoice(Id.generate(), clientData);
-		InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
-		Money price = new Money(10);
-		ProductData productData = new ProductData(Id.generate(), price, "not important", ProductType.DRUG, new Date());
-		RequestItem item = new RequestItem(productData, 0, new Money(10));
-		invoiceRequest.add(item);
-		invoiceRequest.add(item);
+	public final void testIssuance_testState_methodFactoryFromInvoiceFactoryShouldBeAlwaysInvokedOnce() {
+		InvoiceRequest invoiceRequest = new InvoiceRequestBuilder().build();
+		RequestItem requestItem = new RequestItemBuilder().build();
+		invoiceRequest.add(requestItem);
 
-		InvoiceFactory invoiceFactory = Mockito.mock(InvoiceFactory.class);
-		TaxPolicy taxPolicy = Mockito.mock(TaxPolicy.class);
-		Mockito.when(invoiceFactory.create(invoiceRequest.getClientData())).thenReturn(invoice);
-		Mockito.when(taxPolicy.calculateTax(ProductType.DRUG, price)).thenReturn(
-				new Tax(new Money(1d), "not important"));
+		Invoice invoice = new InvoiceBuilder().withClient(invoiceRequest.getClient()).build();
+		Tax tax = new TaxBuilder().build();
 
+		// when
+		Mockito.when(invoiceFactory.create(invoiceRequest.getClientData())).
+				thenReturn(invoice);
+		Mockito.when(taxPolicy.calculateTax(requestItem.getProductData().getType(),
+				requestItem.getProductData().getPrice())).
+				thenReturn(tax);
+
+		// then
 		bookKeeper = new BookKeeper(invoiceFactory);
-		Invoice newInvoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
-		Mockito.verify(invoiceFactory, Mockito.times(1)).create(clientData);
+		bookKeeper.issuance(invoiceRequest, taxPolicy);
+		Mockito.verify(invoiceFactory, Mockito.times(1)).create(invoiceRequest.getClientData());
 	}
 
 }
